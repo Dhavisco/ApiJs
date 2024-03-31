99f86b420d5a6e4fb9e12ecf88f545d6fadaea47
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-31 23:48:31 +0100

    dummy event output

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 
+const EVENTS = [
+  { id: "e1", title: "Event 1" },
+  { id: "e2", title: "Event 2" },
+  { id: "e3", title: "Event 3" },
+];
 
 const EventsPage = () => {
   return (
-    <div>EventsPage</div>
-  )
+    <>
+      <h1> EventsPage</h1>
+      <ul>
+{EVENTS.map(eve => <li key={eve.id}>
+<Link to ={eve.id}>{eve.title}</Link>
+</li>)}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage
